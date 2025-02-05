8125254dcafcd75964005cbecf4b3a70dca2986a
Shohruxshoh <78140473+Shohruxshoh@users.noreply.github.com>
2025-02-05 23:21:06 +0500

    Update serializers.py
    1

--- posts/serializers.py
+++ posts/serializers.py
@@ -19,3 +19,31 @@ class PostCreateSerializer(serializers.ModelSerializer):
         fields = ['category', 'name', 'description', 'image']
 
 
+class PostSaveSerializer(serializers.ModelSerializer):
+    post = PostSerializer(read_only=True)
+    class Meta:
+        model = PostSave
+        fields = ['id', 'post', 'is_save']
+
+class PostSaveCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostSave
+        fields = ['is_save']
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source='user.username')
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'message']
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['user', 'post','message']
+    
+class CommentUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['message']
+
+
