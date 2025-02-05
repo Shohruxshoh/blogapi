561d3a4d5708b3d1c19feebf0540792bf18123c6
Shohruxshoh <78140473+Shohruxshoh@users.noreply.github.com>
2025-02-05 23:22:22 +0500

    Update urls.py
    3

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import CategoryListOrCreateView, CategoryRetrieveUpdateDestroyView, PostListOrCreateView, PostRetrieveUpdateDestroyView
+from .views import CategoryListOrCreateView, CategoryRetrieveUpdateDestroyView, PostListOrCreateView, PostRetrieveUpdateDestroyView, \
+PostSaveListView, PostSaveCreateView, CommentListCreateView, CommentRetrieveUpdateDestroyView
 urlpatterns = [
     path('categories/', CategoryListOrCreateView.as_view(), name='category-list-create'),
     path('categories/<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view(), name='category-retrieve-update-destroy'),
     path('posts/', PostListOrCreateView.as_view(), name='post-list-create'),
     path('posts/<int:pk>/', PostRetrieveUpdateDestroyView.as_view(), name='post-retrieve-update-destroy'),
+    path('post-save/', PostSaveListView.as_view(), name='post-save'),
+    path('post-save-create/<int:post_id>', PostSaveCreateView.as_view(), name='post-save-create'),
+
+    path('comments/<int:pk>', CommentListCreateView.as_view(), name='comments'),
+    path('comments/datail/<int:pk>/', CommentRetrieveUpdateDestroyView.as_view(), name='comment-retrieve-update-destroy'),
 ]
